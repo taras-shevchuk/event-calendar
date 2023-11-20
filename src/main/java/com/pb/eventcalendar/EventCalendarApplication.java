0a35f6a829dd664bf31f0e342967bb715d8304a8
taras-shevchuk <tara7777@ukr.net>
2023-11-20 15:02:32 +0200

    set up

---
+++ src/main/java/com/pb/eventcalendar/EventCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.pb.eventcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCalendarApplication.class, args);
+	}
+}
